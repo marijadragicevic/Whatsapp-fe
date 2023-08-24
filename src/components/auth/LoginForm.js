b4435565132fc5e5552c51236dbc97e8c73dcf73
marija.dragicevic <majadragicevic.111@gmail.com>
2023-08-24 16:39:22 +0200

    Implemented login functonality and styled login page

--- src/components/auth/LoginForm.js
+++ src/components/auth/LoginForm.js
@@ -1,6 +1,33 @@
+import { Link, useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useDispatch, useSelector } from "react-redux";
+import { PulseLoader } from "react-spinners";
+import { signInSchema } from "../../utils/validation";
+import { loginUser } from "../../features/UserSlice";
+import AuthInput from "./AuthInput";
+
 const LoginForm = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const { status, error } = useSelector((state) => state.user);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: yupResolver(signInSchema) });
+
+  const onSubmit = async (data) => {
+    let response = await dispatch(loginUser({ ...data }));
+    if (response.payload.user) {
+      navigate("/");
+    }
+  };
+
   return (
-    <div className="h-screen w-full flex items-center justify-center">
+    <div className="min-h-screen w-full flex items-center justify-center">
       {/* Container */}
       <div className="w-full max-w-md space-y-8 p-10 dark:bg-dark_bg_2 rounded-xl">
         {/* Heading */}
@@ -9,7 +36,49 @@ const LoginForm = () => {
           <p className="mt-2 text-sm">Sign in</p>
         </div>
         {/* Form */}
-        <form className="mt-6 space-y-6"></form>
+        <form className="mt-6 space-y-6" onSubmit={handleSubmit(onSubmit)}>
+          <AuthInput
+            name="email"
+            type="text"
+            placeholder="Email address"
+            register={register}
+            error={errors?.email?.message}
+          />
+          <AuthInput
+            name="password"
+            type="password"
+            placeholder="Password"
+            register={register}
+            error={errors?.password?.message}
+          />
+          {/* if we have an error */}
+          {error && (
+            <div>
+              <p className="text-red-400">{error}</p>
+            </div>
+          )}
+          {/* Submit button */}
+          <button
+            type="submit"
+            className="w-full flex justify-center bg-green_1 text-gray-100 p-4 rounded-full tracking-wide font-semibold focus:outline-none hover:bg-green_2 shadow-lg cursor-pointer transition ease-in duration-300"
+          >
+            {status === "loading" ? (
+              <PulseLoader color="#fff" size={16} />
+            ) : (
+              "Sign in"
+            )}
+          </button>
+          {/* Sign in link */}
+          <p className="flex flex-col justify-center mt-10 text-center text-md dark:text-dark_text_1">
+            <span>You do not have an account ?</span>
+            <Link
+              to="/register"
+              className="hover:underline transition ease-in dura300"
+            >
+              Sign up
+            </Link>
+          </p>
+        </form>
       </div>
     </div>
   );
--- src/utils/validation.js
+++ src/utils/validation.js
@@ -21,3 +21,11 @@ export const signUpSchema = Yup.object({
       "Password must contain atleast 6 characters, 1 uppercase, 1 lowercase, 1 number and 1 special character"
     ),
 });
+
+export const signInSchema = Yup.object({
+  email: Yup.string()
+    .trim()
+    .required("Email address is required")
+    .email("Invalid email"),
+  password: Yup.string().required("Password is required."),
+});
