917e23c03c39849efc51a340440832bdc087fe60
marija.dragicevic <majadragicevic.111@gmail.com>
2023-08-24 17:06:38 +0200

    Added protected routes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,40 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import { useSelector } from "react-redux";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 
-function App() {
+const App = () => {
+  const { user } = useSelector((state) => state.user);
+  const { access_token } = user;
+
+  console.log(user);
+
   return (
     <div className="dark">
       <Router>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
+          <Route
+            path="/"
+            element={access_token ? <Home /> : <Navigate to="/login" />}
+          />
+          <Route
+            path="/login"
+            element={access_token ? <Navigate to="/" /> : <Login />}
+          />
+          <Route
+            path="/register"
+            element={access_token ? <Navigate to="/" /> : <Register />}
+          />
         </Routes>
       </Router>
     </div>
   );
-}
+};
 
 export default App;
