02a38079a5187ce168747c14c1f8dbc980546f7e
marija.dragicevic <majadragicevic.111@gmail.com>
2023-08-25 15:51:51 +0200

    Added sidebar notification component

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,10 +1,13 @@
 import { SidebarHeader } from "./header";
+import { Notifications } from "./notifications";
 
 const Sidebar = () => {
   return (
     <div className="w-[40%] h-full select-none">
       {/* sidebar header */}
       <SidebarHeader />
+      {/* notifications */}
+      <Notifications />
     </div>
   );
 };
