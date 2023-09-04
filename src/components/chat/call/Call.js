cf8c7094ad82a01f7761f1a753a0274c956885ee
marija.dragicevic <majadragicevic.111@gmail.com>
2023-09-04 11:39:28 +0200

    Added call actions component

--- src/components/chat/call/Call.js
+++ src/components/chat/call/Call.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import CallActions from "./CallActions";
 import CallArea from "./CallArea";
 import Header from "./Header";
 import Ringing from "./Ringing";
@@ -5,9 +7,13 @@ import Ringing from "./Ringing";
 const Call = ({ call, setCall, callAccepted }) => {
   const { receivingCall, callEnded } = call;
 
+  const [showActions, setShowActions] = useState(true);
+
   return (
     <div
       className={`fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[350px] h-[550px] z-10 rounded-2xl overflow-hidden callbg`}
+      onMouseOver={() => setShowActions(true)}
+      onMouseOut={() => setShowActions(false)}
     >
       {/* Container */}
       <div>
@@ -15,7 +21,9 @@ const Call = ({ call, setCall, callAccepted }) => {
           {/* Header */}
           <Header />
           {/* Call area */}
-          <CallArea name="Name" />
+          <CallArea name="name" />
+          {/* Call actions */}
+          {showActions && <CallActions />}
         </div>
       </div>
       {/* Ringing */}
